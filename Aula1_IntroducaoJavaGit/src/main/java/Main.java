450b8a8a1407cc80432586a01eec9410dd486f84
MateusuMelo <mateus.c@ges.inatel.br>
2023-02-14 11:11:47 -0300

    Primeiro Codigo

--- Aula1_IntroducaoJavaGit/src/main/java/Main.java
+++ Aula1_IntroducaoJavaGit/src/main/java/Main.java
@@ -1,9 +1,35 @@
+
+import java.util.Scanner;
+
 public class Main {
-    public static class main{
 
-        
+    public static void main(String[] args) {
+
+        System.out.println("BEM VINDO AO ALMOXERIFADO!!");
+        System.out.println("RESISTOR | CAPACITOR | PROTOBOARD");
+        System.out.println("FACA SUA REQUISICAO:");
+
+        // SISTEMA DE ALMOXERIFADO
+
+        Scanner in = new Scanner(System.in);
+        String componente = in.nextLine();
+
+        switch (componente) {
+            case "RESISTOR":
+                System.out.println("Requisitou um resistor");
+                break;
+
+            case "CAPACITOR":
+                System.out.println("Requisitou um capacitor");
+                break;
+
+            case "PROTOBOARD":
+                System.out.println("Requisitou um protoboard");
+                break;
+
+            default:
+                System.out.println("INVALIDO");
+                break;
+        }
     }
-
-
-
 }
